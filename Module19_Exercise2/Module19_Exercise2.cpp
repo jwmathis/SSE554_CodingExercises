208be66b1a6bcb06e14b27c3b470a4c245b6ef6a
jwmathis <jwmathis7@gmail.com>
2025-01-10 20:22:08 -0500

    final updates to module 19 exercises

--- Module19_Exercise2/Module19_Exercise2.cpp
+++ Module19_Exercise2/Module19_Exercise2.cpp
@@ -36,44 +36,50 @@ public:
 
     void append_node(string d) {
         Node* new_node = new Node (d);
+
+        // Initialize list if empty
         if (head == NULL) {
             head = tail = curr = new_node;
         }
         else {
-            //append to the end
-            tail->next = new_node;
-            new_node->prev = tail;
-            tail = new_node;
+            //append node to the end of the list
+            tail->next = new_node; // update tail to point to new_node
+            new_node->prev = tail; // update new_node to point back to previous node
+            tail = new_node; // update what is considered the tail of the list
         }
     }
+
+    // Method to flip one page forward
     void moveForward() {
         if (curr == NULL) {
             cout << "No page content found." << endl;
             return;
         }
         if (curr->next != NULL) {
-            curr = curr->next;
-            cout << curr->data << endl;
+            curr = curr->next; // update current page to the next page
+            cout << curr->data << endl; // print content of next page
         }
-        else {
+        else { // error handling if end of list is reached
             cout << "You are at the last page.\n\n" << curr->data << endl;
         }
     }
 
+    // Method to flip one page back
     void moveBackward() {
         if (curr == NULL) {
             cout << "No page content found." << endl;
             return;
         }
         if (curr->prev != NULL) {
-            curr = curr->prev;
-            cout << curr->data << endl;
+            curr = curr->prev; // update current page to the previous page content
+            cout << curr->data << endl; // show content on update current page
         }
-        else {
+        else { // error handling if beginning of list is reached
             cout << "You are at the first page.\n\n" << curr->data << endl;
         }
     }
 
+    // Method to display 1st page of the book
     void display_current() {
         if (curr == NULL) {
             cout << "No content to display." << endl;
@@ -85,6 +91,7 @@ public:
 
 };
 
+// Function to clear terminal screen
 void clearScreen() {
     cout << "\033[2J\033[1;1H";
 }
@@ -92,34 +99,84 @@ void clearScreen() {
 
 int main()
 {
+    // Initialization of doubly linked list
     DoublyLinkedList my_book;
-    bool flag = true;
+    bool flag = true; // flag to exit book
 
     // User instruction for navigation
     cout << "Use the arrow keys to navigate the book pages or press 'q' to quit.\n\n";
 
     // add pages to the book
-    my_book.append_node("Once upon a time there lived in a certain village a little country girl, the prettiest creature who was ever seen. Her mother was excessively fond of her; and her grandmother doted on her still more. This good woman had a little red riding hood made for her. It suited the girl so extremely well that everybody called her Little Red Riding Hood. ");
-    my_book.append_node("One day her mother, having made some cakes, said to her, \"Go, my dear, and see how your grandmother is doing, for I hear she has been very ill.Take her a cake, and this little pot of butter.\" ");
-    my_book.append_node("Little Red Riding Hood set out immediately to go to her grandmother, who lived in another village. ");
-    my_book.append_node("As she was going through the wood, she met with a wolf, who had a very great mind to eat her up, but he dared not, because of some woodcutters working nearby in the forest. He asked her where she was going. The poor child, who did not know that it was dangerous to stay and talk to a wolf, said to him, \"I am going to see my grandmother and carry her a cake and a little pot of butter from my mother.\" ");
+    my_book.append_node("Once upon a time there lived in a certain village a little country girl, the prettiest creature who was ever seen. "
+        "Her mother was excessively fond of her; and her grandmother doted on her still more. This good woman had a little red riding hood made "
+        "for her. It suited the girl so extremely well that everybody called her Little Red Riding Hood.\n"
+        "One day her mother, having made some cakes, said to her, \"Go, my dear, and see how your grandmother is doing, for I hear she has been "
+        "very ill. Take her a cake, and this little pot of butter.\"\n"
+        "Little Red Riding Hood set out immediately to go to her grandmother, who lived in another village.");
+
+    my_book.append_node("As she was going through the wood, she met with a wolf, who had a very great mind to eat her up, but he dared not, "
+        "because of some woodcutters working nearby in the forest. He asked her where she was going. The poor child, who did not know that it "
+        "was dangerous to stay and talk to a wolf, said to him, \"I am going to see my grandmother and carry her a cake and a little pot of butter "
+        "from my mother.\"\n"
+        "\"Does she live far off?\" said the wolf. \"Oh I say,\" answered Little Red Riding Hood; \"it is beyond that mill you see there, at the "
+        "first house in the village.\"\n"
+        "\"Well,\" said the wolf, \"and I'll go and see her too. I'll go this way and go you that, and we shall see who will be there first.\""
+        "The wolf ran as fast as he could, taking the shortest path, and the little girl took a roundabout way, entertaining herself by gathering "
+        "nuts, running after butterflies, and gathering bouquets of little flowers.It was not long before the wolf arrived at the old woman's house. "
+        "He knocked at the door: tap, tap.");
+
+    my_book.append_node("\"Who's there?\" \"Your grandchild, Little Red Riding Hood,\" replied the wolf, counterfeiting her voice; \"who has brought you "
+        "a cake and a little pot of butter sent you by mother.\"\n"
+        "The good grandmother, who was in bed, because she was somewhat ill, cried out, \"Pull the bobbin, and the latch will go up.\"\n"
+        "The wolf pulled the bobbin, and the door opened, and then he immediately fell upon the good woman and ate her up in a moment, for it been "
+        "more than three days since he had eaten.He then shut the door and got into the grandmother's bed, expecting Little Red Riding Hood, who "
+        "came some time afterwards and knocked at the door: tap, tap. \"Who's there?\"\n");
+
+    my_book.append_node("Little Red Riding Hood, hearing the big voice of the wolf, was at first afraid; but believing her grandmother had a cold "
+        "and was hoarse, answered, \"It is your grandchild Little Red Riding Hood, who has brought you a cake and a little pot of butter mother "
+        "sends you.\" The wolf cried out to her, softening his voice as much as he could, \"Pull the bobbin, and the latch will go up.\""
+        "Little Red Riding Hood pulled the bobbin, and the door opened. The wolf, seeing her come in, said to her, hiding himself under the "
+        "bedclothes, \"Put the cake and the little pot of butter upon the stool, and come get into bed with me.\"");
+
+    my_book.append_node("Little Red Riding Hood took off her clothes and got into bed. She was greatly amazed to see how her grandmother looked "
+        "in her nightclothes, and said to her, \"Grandmother, what big arms you have!\"\n"
+
+        "\"All the better to hug you with, my dear.\"\n"
+
+        "\"Grandmother, what big legs you have!\"\n"
+
+        "\"All the better to run with, my child.\"\n"
+
+        "\"Grandmother, what big ears you have!\"\n"
+
+        "\"All the better to hear with, my child.\"\n"
+
+        "\"Grandmother, what big eyes you have!\"\n"
+
+        "\"All the better to see with, my child.\"\n"
+
+        "\"Grandmother, what big teeth you have got!\"\n"
+
+        "\"All the better to eat you up with.\"\n"
+
+        "And, saying these words, this wicked wolf fell upon Little Red Riding Hood, and ate her all up.");
 
     // Display initial page
     my_book.display_current();
 
 
     do {
-        int key = _getch(); // Get key input
-        if (key == 224) { // Check if its special character
-            key = _getch(); // Get specific arrow key
+        int key = _getch(); // Get keyboard input
+        if (key == 224) { // Check if the keyboard input is a special character
+            key = _getch(); // Get specific code for arrow keys
             clearScreen(); // Clear screen
 
             switch (key) {
-            case 75: // Left arrow
+            case 75: // if key press is Left arrow
                 my_book.moveBackward();
                 break;
 
-            case 77: // Right arrow
+            case 77: // if key press is Right arrow
                 my_book.moveForward();
                 break;
 
@@ -133,6 +190,7 @@ int main()
         }
     } while (flag);
 
+    clearScreen();
     cout << "Thanks for reading!" << endl;
 }
 
--- Module19_Exercise3/Module19_Exercise3.cpp
+++ Module19_Exercise3/Module19_Exercise3.cpp
@@ -11,7 +11,7 @@ void clearScreen();
 
 class Node {
 public:
-    string data; // game category
+    string data;
     Node* next;
 
     Node(string d) {
@@ -24,10 +24,12 @@ class CircularLinkedList {
 public:
     Node* head;
     Node* tail;
+    Node* curr; // used for determining player's turn
 
     CircularLinkedList() {
         head = NULL;
         tail = NULL;
+        curr = NULL;
     }
 
     void append_node(string d) {
@@ -36,6 +38,7 @@ public:
             head = new_node; // initalize head with the first node
             tail = new_node; // update tail
             tail->next = head; // complete circular link
+            curr = head;
         }
         else {
             tail->next = new_node; // update previous node to point to added node
@@ -45,13 +48,22 @@ public:
         }
     }
 
+    // Method to cycle through players
+    void nextPlayer() {
+        if (curr == NULL) {
+            cout << "No players!" << endl;
+        }
+        else {
+            cout << "\n" << curr->data << "'s turn: \n" << endl;
+            curr = curr->next;
+        }
+    }
     string spin_wheel(int spins) {
         Node* temp = head;
         int remaining_speed = spins;
 
         while (remaining_speed > 0) {
-            clearScreen();
-            cout << temp->data << endl; // Show category
+            cout << "Spinning...Current subject:" << temp->data << endl; // Show category
             temp = temp->next; // Update category
             remaining_speed--; // decrease speed
             this_thread::sleep_for(chrono::milliseconds(100)); // small delay for simulating slowing down
@@ -69,7 +81,9 @@ void clearScreen() {
 
 int main()
 {
+    int choice;
     int value;
+    bool flag = true;
 
     CircularLinkedList wheel;
     wheel.append_node("Math");
@@ -78,22 +92,36 @@ int main()
     wheel.append_node("Geography");
     wheel.append_node("Pop Culture");
 
-    // Initialize random seed
-    srand(time(0));
-    cout << "THe categories are: Math, Science, History, Geography, and Pop Culture.\n";
-    cout << "Type \"1\" when you are ready to spin the wheel to select a category. ";
+    CircularLinkedList players;
+    cout << "Enter number of players: ";
     cin >> value;
 
-    if (value == 1) {
-        int initial_speed = rand() % 10 + 10;
-        /*cout << "Spin with initial speed: " << initial_speed << endl;*/
-        string result = wheel.spin_wheel(initial_speed);
-        clearScreen();
-        cout << "Final Category is: " << result << endl;
+    // Add players to circular linked list players
+    for (int i = 1; i <= value; i++) {
+        char str[100];
+        sprintf_s(str, "Player %d", i);
+        players.append_node(str);
     }
-    else {
-        cout << "Thanks for playing!" << endl;
-        return 0;
-    }
+
+    // Initialize random seed
+    srand(time(0));
+    cout << "The categories are: Math, Science, History, Geography, and Pop Culture.\n";
+
+    do {
+        players.nextPlayer();
+        cout << "Type \"1\" to spin the wheel to select a category. ";
+        cin >> choice;
+
+        if (choice == 1) {
+            int initial_speed = rand() % 30;
+            cout << "Spinning with initial speed of: " << initial_speed << endl;
+            string result = wheel.spin_wheel(initial_speed);
+            cout << "Final Category is: " << result << endl;
+        }
+        else {
+            cout << "Thanks for playing!" << endl;
+            flag = false;
+        }
+    } while (flag);
 }
 
