902c78d74c237875e7679631ccc3db511800f1f2
jwmathis7 <jwmathis7@gmail.com>
2025-01-15 07:40:11 -0500

    class for non-binary construction

--- Module21_Exercise1/Module21_Exercise1.cpp
+++ Module21_Exercise1/Module21_Exercise1.cpp
@@ -4,47 +4,63 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Node {
 public:
-	int data;
-	Node* left;
-	Node* right;
+	string data;
+	vector<Node*> children;
 
-	Node(int data, Node* left = NULL, Node* right = NULL) {
+	Node(string data) {
 		this->data = data;
-		this->left = left;
-		this->right = right;
+	}
+
+	void add_child(Node* child) {
+		children.push_back(child);
 	}
 };
 
 void print_tree(Node* tree, int level = 0) {
 	string s = "";
-	if (tree != NULL) {
-		print_tree(tree->right, level + 1);
+	if (tree != nullptr) {
+		for (int i = tree->children.size() - 1; i >= 0; --i) {
+			print_tree(tree->children[i], level + 1);
+		}
 
 		for (int i = 0; i < level; i++) {
 			s += "| ";
 		}
-		s += to_string(tree->data);
+
+		s += tree->data;
 		cout << s << endl;
-		print_tree(tree->left, level + 1);
+
+		for (Node* child : tree->children) {
+			print_tree(child, level + 1);
+		}
 	}
 }
 
 int main()
 {
-	Node* tree = new Node(5);
+	Node* tree = new Node("A");
 
-	tree->left = new Node(2);
-	tree->right = new Node(8);
+	tree->add_child(new Node("B"));
+	tree->add_child(new Node("C"));
 
-	tree->left->left = new Node(1);
-	tree->left->right = new Node(3);
-	tree->right->left = new Node(7);
-	tree->right->right = new Node(10);
+	Node* nodeB = tree->children[0];
+	nodeB->add_child(new Node("D"));
+	nodeB->add_child(new Node("E"));
+	nodeB->add_child(new Node("F"));
+
+	Node* nodeE = nodeB->children[1];
+	nodeE->add_child(new Node("NULL"));
+	nodeE->add_child(new Node("NULL"));
+	nodeE->add_child(new Node("EXIT"));
+
+
+
 	print_tree(tree);
 	return 0;
 }
